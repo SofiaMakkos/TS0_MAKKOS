3cb8afcbef33e730965c3af065ca7a069baaa90b
SofiaMakkos <makkossofia@gmail.com>
2026-09-13 13:13:45 -0300

    jupyter TS3 terminado

--- TS3/.ipynb_checkpoints/TS3juntos-checkpoint.py
+++ TS3/.ipynb_checkpoints/TS3juntos-checkpoint.py
@@ -22,14 +22,8 @@ def mi_funcion_sen( vmax = np.sqrt(2), dc = 0, ff = 1, ph=0, nn = N, fs = fs):
     xx=vmax*np.sin(2*np.pi*ff*tt +ph)+dc
     return tt, xx
 
-def fastfouriertrans (xx, N, fs):
-    fftxx = fft(xx)
-    #fftxx = fftxx[:N//2]
-    freq = fftfreq(N,1/fs)
-    freq = freq[:N//2]
-    fftxx_abs = np.abs(fftxx)
-    return freq, fftxx_abs
-#%%
+#%% k0=N/4
+
 tt1, sen1=mi_funcion_sen(ff=f0)
 
 plt.figure(1)
@@ -58,6 +52,13 @@ plt.title("Seno k0=N/4+0.5")
 plt.ylabel("Amplitud [V]")
 plt.xlabel("Tiempo [s]")
 #%%
+def fastfouriertrans (xx, N, fs):
+    fftxx = fft(xx)
+    #fftxx = fftxx[:N//2]
+    freq = fftfreq(N,1/fs)
+    freq = freq[:N//2]
+    fftxx_abs = np.abs(fftxx)
+    return freq, fftxx_abs
 
 freq1, fftsen1_abs=fastfouriertrans(sen1, N, fs)
 freq2, fftsen2_abs=fastfouriertrans(sen2, N, fs)
@@ -82,24 +83,6 @@ plt.figure(6)
 plt.plot(freq3, fftsen3_abs_dB[:N//2],":.", color="blue")
 plt.title("FFT normalizada Seno k0=N/4+0.5")
 plt.ylabel("Densidad espectral de potencia [dB]")
-plt.xlabel("Frecuencia [Hz]")
-
-plt.figure(7)
-plt.plot(freq1, fftsen1_abs[:N//2]/N, ":.", color="red")
-plt.title("FFT normalizada Seno k0=N/4")
-plt.ylabel("Densidad espectral de potencia [V]")
-plt.xlabel("Frecuencia [Hz]")
-
-plt.figure(8)
-plt.plot(freq2, fftsen2_abs[:N//2]/N,":.", color="green")
-plt.title("FFT normalizada Seno k0=N/4+0.25")
-plt.ylabel("Densidad espectral de potencia [V]")
-plt.xlabel("Frecuencia [Hz]")
-
-plt.figure(9)
-plt.plot(freq3, fftsen3_abs[:N//2]/N, ":.", color="blue")
-plt.title("FFT normalizada Seno k0=N/4+0.5")
-plt.ylabel("Densidad espectral de potencia [V]")
 plt.xlabel("Frecuencia [Hz]")
 
 Psen1=np.sum(sen1**2)/N
@@ -130,51 +113,41 @@ fftsen1_0_abs_dB=20*np.log10(fftsen1_0_abs/ref)
 fftsen2_0_abs_dB=20*np.log10(fftsen2_0_abs/ref)
 fftsen3_0_abs_dB=20*np.log10(fftsen3_0_abs/ref)
 
-plt.figure(10)
+plt.figure(7)
 plt.plot(freq1, fftsen1_0_abs_dB[:10*N//2], ":.", color="red")
 plt.title("FFT normalizada Seno k0=N/4 (zero padding)")
 plt.ylabel("Densidad espectral de potencia [dB]")
 plt.xlabel("Frecuencia [Hz]")
-plt.figure(11)
+plt.figure(8)
 plt.plot(freq2, fftsen2_0_abs_dB[:10*N//2],":.", color="green")
 plt.title("FFT normalizada Seno k0=N/4+0.25 (zero padding)")
 plt.ylabel("Densidad espectral de potencia [dB]")
 plt.xlabel("Frecuencia [Hz]")
-plt.figure(12)
+plt.figure(9)
 plt.plot(freq3, fftsen3_0_abs_dB[:10*N//2], ":.", color="blue")
 plt.title("FFT normalizada Seno k0=N/4+0.5 (zero padding)")
 plt.ylabel("Densidad espectral de potencia [dB]")
 plt.xlabel("Frecuencia [Hz]")
 
-plt.figure(13)
+plt.figure(10)
 plt.plot(freq1, fftsen1_0_abs[:10*N//2]/N, ":.", color="red")
 plt.title("FFT normalizada Seno k0=N/4 (zero padding)")
 plt.ylabel("Densidad espectral de potencia [V]")
 plt.xlabel("Frecuencia [Hz]")
 
-plt.figure(14)
+plt.figure(11)
 plt.plot(freq2, fftsen2_0_abs[:10*N//2]/N,":.", color="green")
 plt.title("FFT normalizada Seno k0=N/4+0.25 (zero padding)")
 plt.ylabel("Densidad espectral de potencia [V]")
 plt.xlabel("Frecuencia [Hz]")
 
-plt.figure(15)
+plt.figure(12)
 plt.plot(freq3, fftsen3_0_abs[:10*N//2]/N, ":.", color="blue")
 plt.title("FFT normalizada Seno k0=N/4+0.5 (zero padding)")
 plt.ylabel("Densidad espectral de potencia [V]")
 plt.xlabel("Frecuencia [Hz]")
 
-Psen1=np.sum(sen1_0**2)/(10*N)
-Pfft_sen1=np.sum(fftsen1_0_abs**2)/(10*N)**2
-print(Psen1, Pfft_sen1)
-
-Psen2=np.sum(sen2_0**2)/(10*N)
-Pfft_sen2=np.sum(fftsen2_0_abs**2)/(10*N)**2
-print(Psen2, Pfft_sen2)
-
-Psen3=np.sum(sen3_0**2)/(10*N)
-Pfft_sen3=np.sum(fftsen3_0_abs**2)/(10*N)**2
-print(Psen3, Pfft_sen3)
+plt.show()
 
 
 
