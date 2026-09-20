01c00baebe94a3c1d2dac089369f4da1c25af93c
SofiaMakkos <makkossofia@gmail.com>
2026-09-19 23:54:40 -0300

    jupyter terminado

--- TS4/TS4.py
+++ TS4/TS4.py
@@ -14,9 +14,7 @@ fs=1000
 
 def mi_fft(x, fs=fs):
     x_fft = np.fft.fft(x, axis=1)
-    
     x_fft = x_fft[:,:N//2]
-    
     freq = np.fft.fftfreq(N, 1/fs)[:N//2]
     x_fft_abs = np.abs(x_fft)
     
@@ -31,40 +29,36 @@ s = np.sqrt(2)*np.sin(np.outer(omega_uno,nn))
 Ps = np.var(s)
 SNR = 3
 sigma = np.sqrt(Ps/np.power(10,SNR/10)) 
-
 n_a = np.random.normal(scale=sigma,size=s.shape)
-    
+
 x = s + n_a
 
 #%% Rectangular 3dB
 freq, x_fft_abs = mi_fft(x)
 x_fft_abs = 2*x_fft_abs/N
 x_fft_abs_traspuesta = np.transpose(x_fft_abs)
+
 plt.figure(1)
 plt.plot(freq,x_fft_abs_traspuesta,":.")
 plt.xlim(240,260)
+plt.title("F1: Senoidales + ruido con ventana implícita (3dB)")
+plt.xlabel("Frecuencia [Hz]")
+plt.ylabel("Densidad espectral de potencia [V]")
+
 a_1_rect = x_fft_abs[:,freq==(omega_cero/(2*np.pi))*N]
 
-
 indice_maximo = np.argmax(x_fft_abs, axis=1)
-omega_rect = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp rectang:", np.mean(a_1_rect))
-print("Media estimador de fre rectang:",np.mean(omega_rect))
-"""
-espa_1_rect = np.mean(a_1_rect)
-sa_1_rect=espa_1_rect-np.sqrt(2)
-print(sa_1_rect)
-espa_1_rect = np.var(a_1_rect)
-sa_1_rect=espa_1_rect-np.sqrt(2)
-print(sa_1_rect)
-#pot = np.mean(x**2)
-#print("Potencia blackmanh:", pot)
+omega_rect = freq[indice_maximo]*2*np.pi/fs
 
-#par = np.sum(x_fft_abs**2, axis=1) / (2)
-#par = np.mean(par)
+esp_a_1 = np.sum(a_1_rect)/200  #Lo mismo que usar np.mean(a_1_rect)
+s_a_1=esp_a_1-np.sqrt(2)
+var_a_1 = np.sum((a_1_rect-esp_a_1)**2)/200
 
-#print("Potencia Parseval:", par)
-"""
+esp_ohm_1 = np.sum(omega_rect)/200 
+s_ohm_1=np.mean(esp_ohm_1-omega_uno)
+var_ohm_1 = np.sum((omega_rect-esp_ohm_1)**2)/200
+
+
 #%% Flattop 3dB
 
 flat=win.flattop(N)
@@ -76,13 +70,23 @@ x_fft_abs_traspuesta = np.transpose(x_fft_abs)
 plt.figure(2)
 plt.plot(freq,x_fft_abs_traspuesta,":.")
 plt.xlim(240,260)
+plt.title("F2: Senoidales + ruido con ventana Flattop (3dB)")
+plt.xlabel("Frecuencia [Hz]")
+plt.ylabel("Densidad espectral de potencia [V]")
+
+
 a_1_flat = x_fft_abs[:,freq==(omega_cero/(2*np.pi))*N]
 
-
 indice_maximo = np.argmax(x_fft_abs, axis=1)
-omega_flat = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp flattop:",np.mean(a_1_flat))
-print("Media estimador de fre flattop:",np.mean(omega_flat))
+omega_flat = freq[indice_maximo]*2*np.pi/fs
+
+esp_a_1 = np.sum(a_1_flat)/200  
+s_a_1=esp_a_1-np.sqrt(2)
+var_a_1 = np.sum((a_1_flat-esp_a_1)**2)/200
+
+esp_ohm_1 = np.sum(omega_flat)/200 
+s_ohm_1=np.mean(esp_ohm_1-omega_uno)
+var_ohm_1 = np.sum((omega_flat-esp_ohm_1)**2)/200
 
 #%% Blackmanharris 3dB
 
@@ -95,59 +99,75 @@ x_fft_abs_traspuesta = np.transpose(x_fft_abs_norm)
 plt.figure(3)
 plt.plot(freq,x_fft_abs_traspuesta,":.")
 plt.xlim(240,260)
+plt.title("F3: Senoidales + ruido con ventana Blackmanharris (3dB)")
+plt.xlabel("Frecuencia [Hz]")
+plt.ylabel("Densidad espectral de potencia [V]")
+
 a_1_blackh = x_fft_abs_norm[:,freq==(omega_cero/(2*np.pi))*N]
 
 indice_maximo = np.argmax(x_fft_abs_norm, axis=1)
-omega_blackh = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp blackmanh:",np.mean(a_1_blackh))
-print("Media estimador de fre blackmanh:",np.mean(omega_blackh))
+omega_blackh = freq[indice_maximo]*2*np.pi/fs
+
+esp_a_1 = np.sum(a_1_blackh)/200 
+s_a_1=esp_a_1-np.sqrt(2)
+var_a_1 = np.sum((a_1_blackh-esp_a_1)**2)/200
+
+esp_ohm_1 = np.sum(omega_blackh)/200 
+s_ohm_1=np.mean(esp_ohm_1-omega_uno)
+var_ohm_1 = np.sum((omega_blackh-esp_ohm_1)**2)/200
+
+
+#%% Hann 3dB
+hann=win.hann(N)
+x_hann=x*hann
+
+freq, x_fft_abs = mi_fft(x_hann)
+x_fft_abs_norm = 2*x_fft_abs/N
+x_fft_abs_traspuesta = np.transpose(x_fft_abs_norm)
+plt.figure(4)
+plt.plot(freq,x_fft_abs_traspuesta,":.")
+plt.xlim(240,260)
+plt.title("F4: Senoidales + ruido con ventana Hann (3dB)")
+plt.xlabel("Frecuencia [Hz]")
+plt.ylabel("Densidad espectral de potencia [V]")
+
+a_1_hann = x_fft_abs_norm[:,freq==(omega_cero/(2*np.pi))*N]
+
+indice_maximo = np.argmax(x_fft_abs_norm, axis=1)
+omega_hann = freq[indice_maximo]*2*np.pi/fs
+
+esp_a_1 = np.sum(a_1_hann)/200
+s_a_1=esp_a_1-np.sqrt(2)
+var_a_1 = np.sum((a_1_hann-esp_a_1)**2)/200
+
+esp_ohm_1 = np.sum(omega_hann)/200 
+s_ohm_1=np.mean(esp_ohm_1-omega_uno)
+var_ohm_1 = np.sum((omega_hann-esp_ohm_1)**2)/200
 
 #%% Histogramas 3dB
 
 plt.figure(5)
-plt.hist(a_1_rect, bins=20, color="red")
-plt.hist(a_1_flat, bins=20, color="blue")
-plt.hist(a_1_blackh, bins=20, color="gold", alpha=0.7)
+plt.hist(a_1_rect, bins=20, color="red", label="Rectangular")
+plt.hist(a_1_hann, bins=20, color="green", label="Hann")
+plt.hist(a_1_blackh, bins=20, color="gold", label="Blackmanharris")
+plt.hist(a_1_flat, bins=20, color="blue", label="Flattop")
+plt.title("F5: Histograma del estimador $\widehat{a}_1^i$ (3dB)")
+plt.xlabel("Amplitud [V]")
+plt.ylabel("Número de resoluciones")
+plt.legend()
 
 plt.figure(6)
-plt.hist(omega_rect, bins=20, color="red", histtype="step", linewidth=2)
-plt.hist(omega_flat, bins=20, color="blue", histtype="step", linewidth=2)
-plt.hist(omega_blackh, bins=20, color="gold", histtype="step", linewidth=2)
-
-"""
-plt.figure(5)
-
-plt.hist(a_1_rect, bins=20, alpha=0.5, color="red", label="Rectangular")
-plt.hist(a_1_flat, bins=20, alpha=0.5, color="blue", label="Flattop")
-plt.hist(a_1_blackh, bins=20, alpha=0.5, color="gold", label="Blackman-Harris")
-
-plt.xlabel("Amplitud")
-plt.ylabel("Cantidad de muestras")
-plt.title("Histograma de amplitudes - SNR = 3 dB")
+plt.hist(omega_rect, bins=20, color="red", histtype="step", linewidth=2, label="Rectangular")
+plt.hist(omega_flat, bins=20, color="blue", histtype="step", linewidth=2, label="Flattop")
+plt.hist(omega_blackh, bins=20, color="gold", histtype="step", linewidth=2, label="Blackmanharris")
+plt.hist(omega_hann, bins=20, color="green", histtype="step", linewidth=2, label="Hann")
+plt.title("F6: Histograma del estimador $\widehat{Ω}_1^i$ (3dB)")
+plt.xlabel(" [Hz]")
+plt.ylabel("Número de resoluciones")
 plt.legend()
-plt.grid(alpha=0.2)
-
-
-plt.figure(6)
-
-plt.hist(omega_rect, bins=20, alpha=0.5, color="red", label="Rectangular")
-plt.hist(omega_flat, bins=20, alpha=0.5, color="blue", label="Flattop")
-plt.hist(omega_blackh, bins=20, alpha=0.5, color="gold", label="Blackman-Harris")
-
-plt.xlabel("Frecuencia")
-plt.ylabel("Cantidad de muestras")
-plt.title("Histograma de frecuencias - SNR = 3 dB")
-plt.legend()
-plt.grid(alpha=0.2)
-
-plt.show()
-"""
-
-
 
 
 #%% Para SNR=10
-
 SNR = 3
 sigma = np.sqrt(Ps/np.power(10,SNR/10)) 
 
@@ -162,13 +182,22 @@ x_fft_abs_traspuesta = np.transpose(x_fft_abs)
 plt.figure(7)
 plt.plot(freq,x_fft_abs_traspuesta,":.")
 plt.xlim(240,260)
+plt.title("F7: Senoidales + ruido con ventana implícita (10dB)")
+plt.xlabel("Frecuencia [Hz]")
+plt.ylabel("Densidad espectral de potencia [V]")
+
 a_1_rect = x_fft_abs[:,freq==(omega_cero/(2*np.pi))*N]
 
 indice_maximo = np.argmax(x_fft_abs, axis=1)
-omega_rect = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp rectang:", np.mean(a_1_rect))
-print("Media estimador de fre rectang:",np.mean(omega_rect))
+omega_rect = freq[indice_maximo]*2*np.pi/fs
 
+esp_a_1 = np.sum(a_1_rect)/200
+s_a_1=esp_a_1-np.sqrt(2)
+var_a_1 = np.sum((a_1_rect-esp_a_1)**2)/200
+
+esp_ohm_1 = np.sum(omega_rect)/200 
+s_ohm_1=np.mean(esp_ohm_1-omega_uno)
+var_ohm_1 = np.sum((omega_rect-esp_ohm_1)**2)/200
 #%% Flattop 10dB
 
 flat=win.flattop(N)
@@ -180,14 +209,22 @@ x_fft_abs_traspuesta = np.transpose(x_fft_abs)
 plt.figure(8)
 plt.plot(freq,x_fft_abs_traspuesta,":.")
 plt.xlim(240,260)
+plt.title("F8: Senoidales + ruido con ventana Flattop (10dB)")
+plt.xlabel("Frecuencia [Hz]")
+plt.ylabel("Densidad espectral de potencia [V]")
+
 a_1_flat = x_fft_abs[:,freq==(omega_cero/(2*np.pi))*N]
 
-
 indice_maximo = np.argmax(x_fft_abs, axis=1)
-omega_flat = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp flattop:",np.mean(a_1_flat))
-print("Media estimador de fre flattop:",np.mean(omega_flat))
+omega_flat = freq[indice_maximo]*2*np.pi/fs
 
+esp_a_1 = np.sum(a_1_flat)/200 
+s_a_1=esp_a_1-np.sqrt(2)
+var_a_1 = np.sum((a_1_flat-esp_a_1)**2)/200
+
+esp_ohm_1 = np.sum(omega_flat)/200 
+s_ohm_1=np.mean(esp_ohm_1-omega_uno)
+var_ohm_1 = np.sum((omega_flat-esp_ohm_1)**2)/200
 #%% Blackmanharris 10dB
 
 blackh=win.blackmanharris(N)
@@ -199,21 +236,71 @@ x_fft_abs_traspuesta = np.transpose(x_fft_abs_norm)
 plt.figure(9)
 plt.plot(freq,x_fft_abs_traspuesta,":.")
 plt.xlim(240,260)
+plt.title("F9: Senoidales + ruido con ventana Blackmanharris (3dB)")
+plt.xlabel("Frecuencia [Hz]")
+plt.ylabel("Densidad espectral de potencia [V]")
+
 a_1_blackh = x_fft_abs_norm[:,freq==(omega_cero/(2*np.pi))*N]
 
 indice_maximo = np.argmax(x_fft_abs_norm, axis=1)
-omega_blackh = freq[indice_maximo]*2*np.pi/N
-print("Media estimador de amp blackmanh:",np.mean(a_1_blackh))
-print("Media estimador de fre blackmanh:",np.mean(omega_blackh))
+omega_blackh = freq[indice_maximo]*2*np.pi/fs
+
+esp_a_1 = np.sum(a_1_blackh)/200 
+s_a_1=esp_a_1-np.sqrt(2)
+var_a_1 = np.sum((a_1_blackh-esp_a_1)**2)/200
+
+esp_ohm_1 = np.sum(omega_blackh)/200 
+s_ohm_1=np.mean(esp_ohm_1-omega_uno)
+var_ohm_1 = np.sum((omega_blackh-esp_ohm_1)**2)/200
+
+#%% Hann 10dB
+hann=win.hann(N)
+x_hann=x*hann
+
+freq, x_fft_abs = mi_fft(x_hann)
+x_fft_abs_norm = 2*x_fft_abs/N
+x_fft_abs_traspuesta = np.transpose(x_fft_abs_norm)
+plt.figure(10)
+plt.plot(freq,x_fft_abs_traspuesta,":.")
+plt.xlim(240,260)
+plt.title("F3: Senoidales + ruido con ventana Hann (10dB)")
+plt.xlabel("Frecuencia [Hz]")
+plt.ylabel("Densidad espectral de potencia [V]")
+
+a_1_hann = x_fft_abs_norm[:,freq==(omega_cero/(2*np.pi))*N]
+
+indice_maximo = np.argmax(x_fft_abs_norm, axis=1)
+omega_hann = freq[indice_maximo]*2*np.pi/fs
+
+esp_a_1 = np.sum(a_1_hann)/200
+s_a_1=esp_a_1-np.sqrt(2)
+var_a_1 = np.sum((a_1_hann-esp_a_1)**2)/200
+
+esp_ohm_1 = np.sum(omega_blackh)/200 
+s_ohm_1=np.mean(esp_ohm_1-omega_uno)
+var_ohm_1 = np.sum((omega_blackh-esp_ohm_1)**2)/200
 
 #%% Histogramas 10dB
 
 plt.figure(11)
-plt.hist(a_1_rect, bins=20, color="red")
-plt.hist(a_1_flat, bins=20, color="blue")
-plt.hist(a_1_blackh, bins=20, color="gold", alpha=0.7)
+plt.hist(a_1_rect, bins=20, color="red", label="Rectangular")
+plt.hist(a_1_hann, bins=20, color="green", label="Hann")
+plt.hist(a_1_blackh, bins=20, color="gold", label="Blackmanharris")
+plt.hist(a_1_flat, bins=20, color="blue", label="Flattop")
+plt.title("F5: Histograma del estimador $\widehat{a}_1^i$ (10dB)")
+plt.xlabel("Amplitud [V]")
+plt.ylabel("Número de resoluciones")
+plt.legend()
 
 plt.figure(12)
-plt.hist(omega_rect, bins=20, color="red", histtype="step", linewidth=2)
-plt.hist(omega_flat, bins=20, color="blue", histtype="step", linewidth=2)
-plt.hist(omega_blackh, bins=20, color="gold", histtype="step", linewidth=2)
+plt.hist(omega_rect, bins=20, color="red", histtype="step", linewidth=2, label="Rectangular")
+plt.hist(omega_flat, bins=20, color="blue", histtype="step", linewidth=2, label="Flattop")
+plt.hist(omega_blackh, bins=20, color="gold", histtype="step", linewidth=2, label="Blackmanharris")
+plt.hist(omega_hann, bins=20, color="green", histtype="step", linewidth=2, label="Hann")
+plt.title("F6: Histograma del estimador $\widehat{Ω}_1^i$ (10dB)")
+plt.xlabel(" [Hz]")
+plt.ylabel("Número de resoluciones")
+plt.legend()
+
+#%% Sesgo y varianza
+
